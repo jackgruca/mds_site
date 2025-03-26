c081898dbddb7667e629bfec6cb0e70d49a8e111
jackgruca <jackgruca@gmail.com>
2025-03-26 02:01:00 -0400

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "5c5dd6cf6bf89e759a8a31e7523efef7",
+const RESOURCES = {"flutter_bootstrap.js": "50344fd54b80cda24a29cd8e7adc7f18",
 "version.json": "a7d279fdf72a0a2396edc1aa45a2bb25",
 "index.html": "ec15e0e5f9ec634b53727f4a0e8d113e",
 "/": "ec15e0e5f9ec634b53727f4a0e8d113e",
-"main.dart.js": "320e41879753d8d1aa551ee179aa65af",
+"main.dart.js": "1ec2e8d3760fa31d3a87f9a8769ffeb2",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
@@ -22,7 +22,7 @@ const RESOURCES = {"flutter_bootstrap.js": "5c5dd6cf6bf89e759a8a31e7523efef7",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "e986ebe42ef785b27164c36a9abc7818",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/AssetManifest.bin": "8ed965680a6ae0bd08ea4aaba1a0ee7d",
-"assets/fonts/MaterialIcons-Regular.otf": "4fe22f0243906d1c43aa95235b429ad4",
+"assets/fonts/MaterialIcons-Regular.otf": "276fa7aecbefba2521a6e35f99492df5",
 "assets/assets/2025/player_descriptions.csv": "c84e63225e32cbf8e42ed981e3b828f7",
 "assets/assets/2025/available_players.csv": "65db7a42ec600027003ada9400113560",
 "assets/assets/2025/team_needs.csv": "2b014932d22e7865767df5953973108b",
