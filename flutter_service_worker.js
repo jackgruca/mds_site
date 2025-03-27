620f22b58094e578994e54fb285b66c3ed2d5613
jackgruca <jackgruca@gmail.com>
2025-03-27 15:44:28 -0400

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "e1397d5b50b0f88481a3315304b84fbc",
+const RESOURCES = {"flutter_bootstrap.js": "0ce8bd49042d8b042ae9f57fe51073a9",
 "version.json": "a7d279fdf72a0a2396edc1aa45a2bb25",
 "index.html": "ec15e0e5f9ec634b53727f4a0e8d113e",
 "/": "ec15e0e5f9ec634b53727f4a0e8d113e",
