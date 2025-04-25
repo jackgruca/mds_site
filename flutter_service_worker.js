7699ba855cc316578dcd749236d6012a953818c1
jackgruca <jackgruca@gmail.com>
2025-04-24 21:40:01 -0400

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "51696b8c24d70c79eabcfd1d9151054f",
+const RESOURCES = {"flutter_bootstrap.js": "9b270eb49914ac3b2695f1a9a28c73a8",
 "version.json": "a7d279fdf72a0a2396edc1aa45a2bb25",
 "index.html": "ec15e0e5f9ec634b53727f4a0e8d113e",
 "/": "ec15e0e5f9ec634b53727f4a0e8d113e",
@@ -27,9 +27,9 @@ const RESOURCES = {"flutter_bootstrap.js": "51696b8c24d70c79eabcfd1d9151054f",
 "assets/AssetManifest.bin": "ecc86e56b27b91423b3e0a35f4feecf3",
 "assets/fonts/MaterialIcons-Regular.otf": "d3e1faad4fcb51687ff668b2d5f262bc",
 "assets/assets/2025/player_descriptions.csv": "54990f243e5dad8d76217558204321d9",
-"assets/assets/2025/available_players.csv": "aa1d25677f730f69f175078ba37685f8",
+"assets/assets/2025/available_players.csv": "7e4e96c045850722d568ee3b8b75fe16",
 "assets/assets/2025/team_needs.csv": "395b55d15fadeb1df2e8d718a36741d6",
-"assets/assets/2025/live_picks.csv": "70db65d2db764c35857e6c00dffb7c66",
+"assets/assets/2025/live_picks.csv": "f47ace0c98c5e05765d2abb9acbe43e5",
 "assets/assets/2025/draft_order.csv": "06ca2f9094f80febcead6d481db58305",
 "assets/assets/2024/available_players.csv": "9a801b237d2fd1926d29733c5d57f1ee",
 "assets/assets/2024/team_needs.csv": "4887cb9e45637441a05c0d2ef84cb3e6",
